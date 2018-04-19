d9e1ae81301b256820f339d709c6688756bf874e
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-04-19 12:52:36 +0100

    Added delete functionality on backspace

--- public/index.js
+++ public/index.js
@@ -2,13 +2,11 @@ const app = function () {
   const canvas = document.getElementById('main-canvas');
   const context = canvas.getContext('2d');
 
-  let startX = 300;
-  let startY = 250;
-  let currentX = startX;
-  let currentY = startY;
+  let currentX = 300;
+  let currentY = 250;
 
   context.beginPath();
-  context.moveTo(startX, startY);
+  context.moveTo(currentX, currentY);
 
   const checkKey = function (event) {
     if (event.key === 'ArrowLeft') {
@@ -19,31 +17,40 @@ const app = function () {
       rightArrow();
     } else if (event.key === 'ArrowDown') {
       downArrow();
+    } else if (event.key === 'Backspace') {
+      clearAll();
     }
   }
 
   const leftArrow = function () {
-    currentX -= 10;
+    currentX -= 5;
     context.lineTo(currentX, currentY);
     context.stroke();
   }
   const rightArrow = function () {
-    currentX += 10;
+    currentX += 5;
     context.lineTo(currentX, currentY);
     context.stroke();
   }
   const upArrow = function () {
-    currentY -= 10;
+    currentY -= 5;
     context.lineTo(currentX, currentY);
     context.stroke();
   }
   const downArrow = function () {
-    currentY += 10;
+    currentY += 5;
     context.lineTo(currentX, currentY);
     context.stroke();
   }
 
+  const clearAll = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+  }
+
   document.addEventListener('keydown', checkKey);
+  document.addEventListener('keypress', checkKey);
+
 }
 
 document.addEventListener('DOMContentLoaded', app);
